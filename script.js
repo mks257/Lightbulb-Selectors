98c3b11bd964ff208353c3b1d834cace77e63f3e
mks257 <138339735+mks257@users.noreply.github.com>
2025-05-04 17:16:05 -0500

    added lightbulb click handler

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Write your code here
+const bulb1 =document.querySelector("#lightbulb1");
+const bulb2 =document.querySelector("#lightbulb2");
+const bulb3 =document.querySelector("#lightbulb3");
+
+const subtitle =document.querySelector(".subtitle");
+
+let count=0;
+function handleClick(bulb) {
+    count+=1;
+    const times = count === 1 ? "time" : "times";
+    subtitle.textContent = `You've clicked the lights ${count} ${times}`;
+    bulb.classList.toggle("active");
+}    
+bulb1.addEventListener("click", () => handleClick(bulb1));
+bulb2.addEventListener("click", () => handleClick(bulb2));
+bulb3.addEventListener("click", () => handleClick(bulb3));
